2b5a5d1dffc046a1fa8c16c610ac445e9bab8517
Shivansh Rustagi <shivrustagi221@gmail.com>
2016-07-27 16:08:23 -0700

    hello

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$("#more").click(function(){
+    $("#info").animate({"left": "0",'opacity':'1'}, "slow");
+    $("")
+});
+
+$("#back").click(function(){
+    $("#info").animate({"right": "100%",'opacity':'1'}, "slow");
+});
