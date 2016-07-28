8424511026ee78180e9f575462e763a0de712649
Shivansh Rustagi <shivrustagi221@gmail.com>
2016-07-27 19:25:32 -0700

    cleared up formatting, resolved everything except ICONS

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 $("#more").click(function(){
-    $("#info").animate({"left": "30%",'opacity':'1'}, 650);
-    $("#info").animate({"left": "0%",'opacity':'1'}, 300);
+    $("section").css({"position": "fixed"});
+    $("#info").animate({"left": "10%",}, 650);
+    $("#info").animate({"left": "0%",}, 300);
 });
 
 $("#back").click(function(){
-    $("#info").animate({"left": "-100%",'opacity':'1'}, "slow");
+	$("section").css({"position": "absolute"});
+    $("#info").animate({"left": "-110%"}, "slow");
 });
