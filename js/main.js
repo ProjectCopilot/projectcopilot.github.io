865b8a3ab5a317cc6bcec60a0aaf4e06f149e215
Shivansh Rustagi <shivrustagi221@gmail.com>
2016-07-28 10:48:11 -0700

    changed home link, changed animation

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 $("#more").click(function(){
     $("section").css({"position": "fixed"});
-    $("#info").animate({"left": "10%",}, 200);
-    $("#info").animate({"left": "0%",}, 200);
+    $("#info").animate({"left": "0%"}, 200);
+    $("#info").animate({"opacity" : "1"}, 190);
 });
 
 $("#back").click(function(){
 	$("section").css({"position": "absolute"});
+    $("#info").animate({"opacity" : "0.5"}, 200);
     $("#info").animate({"left": "-110%"}, 200);
 });
