6552b4e220d411340c3132a91adca384c646d33b
Ankit Ranjan <me@ankit.io>
2016-07-29 14:59:21 -0700

    Update code styling

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-$("#more").click(function(){
-    $("section").css({"position": "fixed"});
-    $("#info").animate({"left": "0%"}, 200);
-    $("#info").animate({"opacity" : "1"}, 190);
+$('#more').click(function() {
+    $('section').css({'position': 'fixed'});
+    $('#info').animate({'left': '0%'}, 200);
+    $('#info').animate({'opacity': '1'}, 190);
 });
 
-$("#back").click(function(){
-	$("section").css({"position": "absolute"});
-    $("#info").animate({"opacity" : "0.5"}, 200);
-    $("#info").animate({"left": "-110%"}, 200);
+$('#back').click(function() {
+	$('section').css({'position': 'absolute'});
+    $('#info').animate({'opacity': '0.5'}, 200);
+    $('#info').animate({'left': '-110%'}, 200);
 });
